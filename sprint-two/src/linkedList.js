09e087c490fa4125773f7fa1d4a6f72b4655f1a4
grantbelcher <gtbelch@gmail.com>
2020-02-07 19:57:02 -0800

    passes tests for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    }
+    // var newHead = list.head.next;
+    var result = list.head.value;
+    // delete list.head;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current !== null) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+
   };
 
   return list;
